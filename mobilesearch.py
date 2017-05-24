7e58ed3488eb7f7b0238a5e01757204badb145d1
Kevin <kevin.mellachew@gmail.com>
2017-05-24 15:07:08 -0400

    Add comments and functionality to iterate through list

--- mobilesearch.py
+++ mobilesearch.py
@@ -1,7 +1,13 @@
 import re
 
+# Add location for the file to search here
 FILE_LOCATION = '2016-17-searches.txt'
 
+# Grab all lines from the file specified
 lines = []
 with open(FILE_LOCATION) as file:
-    lines = file.readline
+    lines = file.readlines()
+
+# Iterate through each line in the list
+for i in range(0, len(lines)):
+    print(lines[i])
