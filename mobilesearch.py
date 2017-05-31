69f6f85a41a99f06f012155085ef879a6474b747
Kevin <kevin.mellachew@gmail.com>
2017-05-31 12:28:01 -0400

    Add pulltext and Rename functions
    Renamed searchfolder to pullfiles for better clarity, and added pulltext function to pull all text from files

--- mobilesearch.py
+++ mobilesearch.py
@@ -2,7 +2,7 @@ import os
 import glob
 
 
-def _foldersearch():
+def _pullfiles():
     '''Searches in the folder that the program is currently in and pulls all txt files into a list'''
     folder = os.path.dirname(os.path.realpath(__file__))
     os.chdir(folder)
@@ -10,10 +10,18 @@ def _foldersearch():
     return files
 
 
+def _pulltext(files):
+    '''Pull all text from each file pulled'''
+    for x in range(0, len(files)):
+        with open(files[x]) as file:
+            text = file.readlines()
+    return text
+
 def init():
     print('Searching files...')
-    files = _foldersearch()
+    files = _pullfiles()
     print('Found ' + str(len(files)) + ' files!')
+    text = _pulltext(files)
 
 
 init()
