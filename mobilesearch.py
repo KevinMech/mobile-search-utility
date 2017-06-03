655b87a07726515de6716b26e508af6dabb2a398
Kevin <kevin.mellachew@gmail.com>
2017-06-02 22:48:09 -0400

    Added cli argument capabilities for specifying folder/file location

--- mobilesearch.py
+++ mobilesearch.py
@@ -1,28 +1,40 @@
 import os
+import sys
 import glob
 
 
-def _pull_files():
+def pull_files(folder):
     '''Searches in the folder that the program is currently in and pulls all txt files into a list'''
-    folder = os.path.dirname(os.path.realpath(__file__))
     os.chdir(folder)
     files = glob.glob('*.txt')
     return files
 
 
-def _pull_text(files):
-    '''Pull all text from each file pulled'''
-    for x in range(0, len(files)):
-        with open(files[x]) as file:
+def pull_text(files, single):
+    '''Extract text from all files specified'''
+    if single:
+        with open(files) as file:
             text = file.readlines()
+    else:
+        for i in range(0, len(files)):
+            with open(files[i]) as file:
+                text = file.readlines()
+    print(text)
     return text
 
 
 def main():
-    print('Searching files...')
-    files = _pull_files()
-    print('Found ' + str(len(files)) + ' files!')
-    text = _pull_text(files)
+    # Check to see whether the first argument is a directory or file.
+    if os.path.isdir(sys.argv[1]):
+        # print('is directory')
+        files = pull_files(sys.argv[1])
+        pull_text(files, False)
+    elif os.path.isfile(sys.argv[1]):
+        # print('is file')
+        file = os.path.basename(sys.argv[1])
+        pull_text(file, True)
+    else:
+        print('Error: Please specifiy either a file or directory in your first argument')
 
 
 if __name__ == '__main__':
